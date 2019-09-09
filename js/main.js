4b92be571440ed96adbfd91cebd63426e7965ead
Jefferson Lima <jeffersonlima94@hotmail.com>
2019-09-09 16:28:48 -0300

    Dicionário, Arrays, Push, Pop, Length e Reverse

--- js/main.js
+++ js/main.js
@@ -1,10 +1,24 @@
-var nome = "Jefferson Lima";
+/*var nome = "Jefferson Lima";
 var idade = 29;
 var idade2 = 10;
-var frase = "Japão é o melhor time do mundo"
+var frase = "Japão é o melhor time do mundo"*/
 //alert(nome + " tem " + idade + " anos");
 //alert(idade + idade2);
 //console.log(frase.replace("Japão", "Brasil"))
 //alert(frase.replace("Japão", "Brasil"))
 //console.log(frase.toUpperCase);
 //console.log(frase.toLowerCase);
+
+//var lista = ["maça", "pêra", "uva" ];
+//console.log(lista.join("-")); Você coloca o que quiser para separa os elementos
+//console.log(lista.toString()); transforma em string
+//lista.push("pêra"); Colocar um elemento na lista usa-se Push
+//lista.pop("uva"); Pop retira o elemento
+//console.log(lista.length);Tamanho da minha lista
+//console.log(lista.reverse()); //Imprimi a lista ao contrario 
+//console.log(lista[0]);
+//alert(lista[0]);
+
+/*var frutas  = [{nome:"maça", cor:"vermelha"}, {nome:"uva", cor:"roxa"}];
+console.log(frutas);
+alert(frutas[0].nome); Usando dicionario para fazer arrays */
